753bd27ead9e040ffbcc4dc82ca6dfb03f7f21ed
Jonny Tiscareno <jonnytiscareno@berkeley.edu>
2018-02-22 17:14:38 -0800

    hmw 2

--- app/controllers/concerns/person.rb
+++ app/controllers/concerns/person.rb
@@ -7,18 +7,15 @@ class Person
   end
 
   def nickname
-    # YOUR IMPLEMENTATION HERE
-    'delete_me'
+    @name[0] + @name[1] + @name[2] + @name[3]
   end
 
   def birth_year
-    # YOUR IMPLEMENTATION HERE
-    1990
+    Time.now.year - @age.to_i
   end
 
   def introduction
-    # YOUR IMPLEMENTATION HERE
-    'delete_me'
+    'My name is ' + @name + ' and I am ' + @age + ' years old.'
   end
 
 end
